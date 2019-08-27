e7dc59e9c973ba8a9091ab1072867197951dd504
aton-puzyr <4nthonymayer@gmail.com>
2019-08-27 15:30:20 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: 'file.csv-loader',
+            loader: 'file-loader',
             options: {
               name: '[path][name].[ext]',
             },
