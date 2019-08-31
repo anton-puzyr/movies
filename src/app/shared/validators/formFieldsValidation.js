ba53ad1421121d4813439d50daa863e3f954e434
aton-puzyr <4nthonymayer@gmail.com>
2019-08-31 12:15:52 +0300

    Feedback fixes

--- src/app/shared/validators/formFieldsValidation.js
+++ src/app/shared/validators/formFieldsValidation.js
@@ -5,3 +5,6 @@ export const number = value => value && isNaN(Number(value)) ? 'Only numbers' : 
 export const minValue3 = value => value && value.length < 3 ? 'String is to short' : undefined;
 
 export const maxValue4 = value => value && value.length > 4 ? 'String is to long' : undefined;
+
+export const yearRange = value =>
+  value < 1850 || value > 2020 ? 'Year should match the range 1850 - 2020' : undefined;
